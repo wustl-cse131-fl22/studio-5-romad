808bcdc5198d144309b358e4f657f1b28bf849c7
romadesai <romadesai@romas-air.dhcp.wustl.edu>
2022-10-20 14:37:47 -0500

    with Nadiyah 10.20

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -16,6 +16,8 @@ public class Methods {
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
+		distance = Math.sqrt((Math.pow((x2-x1),2) + Math.pow(y2-y1, 2)));
+		
 		
 		return distance;
 	}
@@ -34,17 +36,20 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
+		StdDraw.setPenColor(0,109,219);
+		StdDraw.filledCircle(x, y, radius*(3.0/4.0));
+	
 		
-
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
+		StdDraw.setPenColor(146,0,0);
+		StdDraw.filledCircle(x,y,radius*(1.0/2.0));
 
-		
-
+	
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		StdDraw.setPenColor(255,255,109);
+		StdDraw.filledCircle(x,y,radius*(1.0/4.0));
 		
 	}
 
